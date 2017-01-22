e48201bea20bd7e584a1eb9632709e56e071ed11
lthoang@wpi.edu <anhmiuhv@yahoo.com.vn>
2017-01-22 00:39:13 -0500

    add class Robot and class Step

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import math
 
 #parsing option from users
 parser = argparse.ArgumentParser(description='Please give mr.robot a terrain information')
@@ -18,21 +19,80 @@ for line in f.readlines():
     r += 1
     c = len(cols)
 l = [a.strip('\n') for a in l]
+f.close()
 
 #helper function
 def array_pos(row, col):
 	if row >= r or col >= c:
 		return -1
+	return row*c + col
 
-	return row*count + col
+def rowcol_from(arraypos):
+	if arraypos >= len(l):
+		return (-1,-1)
+
+	row = arraypos // c
+	col = arraypos % c
+	return (row, col)
 
 #robot information
-robot_pos = [0 for _ in range(c * r)]
-robot_pos[l.index('S')] = 1
-print robot_pos
+
+
+#node for a star
+class Robot:
+	valid_dir = ["n", "s", "e", "w"]
+	def __init__(self):
+		self.robot_pos = [0 for _ in range(c * r)]
+		self.robot_pos[l.index('S')] = 1
+		self.pos = l.index('S')
+		self.direction = 'n'
+
+	def getcost(self, dis):
+		rowcol = rowcol_from(self.pos)
+		new_rc = (-1,-1)
+		f = math.inf
+		if self.direction == "n":
+			new_rc = (rowcol[0] - dis, rowcol[1])
+			if new_rc[0] < 0:
+				return math.inf
+			f = l[arraypos(new_rc)]
+		elif self.direction == "s":
+			new_rc = (rowcol[0] + dis, rowcol[1])
+			if new_rc[0] >= r:
+				return math.inf
+		elif self.direction == "e":
+			new_rc = (rowcol[0], rowcol[1] + 1)
+			if new_rc[1] >= c:
+				return math.inf
+				
+
+class Step:
+	valid = ["fw", "leap", "left", "right"]
+	def __init__(self,type):
+		if type in valid:
+			self.type = type
+		
+	def cost(robot):
+		rowcol = rowcol_from(robot_pos)
+		if self.type == "leap":
+			#TODO: detect unleapable terrain
+			return 20
+		if robot.direction == "n":
+			new_rc = (rowcol[0] + 1, rowcol[1])
+
+
+
+
+
+
+
+
+
 #main a star function
 
 
+#test
+print (array_pos(1,2))
+print (rowcol_from(5))
 
 
-f.close()
